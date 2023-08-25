b94508343ffdb54b1a4bc78d3af6fab08c384ec3
naivecoder02 <113258660+naivecoder02@users.noreply.github.com>
2023-08-25 18:36:13 +0530

    todoscript.js

--- TodoApp/scripts/todoscript.js
+++ TodoApp/scripts/todoscript.js
@@ -1,125 +1,162 @@
 const btn = document.getElementById('btnId');
 const todoInput = document.getElementById("newTodoInput");
-const prioritySelectorNode= document.getElementById("prioritySelector");
+const prioritySelectorNode = document.getElementById("prioritySelector");
 const ul = document.getElementById("todos");
-// import { v4 as uuidv4 } from 'uuid';
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
+  const todoText = todoInput.value;
+  const priority = prioritySelectorNode.value;
 
-
-const todoText = todoInput.value;
-const priority = prioritySelectorNode.value;
-
-if(!todoText || !priority){
+  if (!todoText || !priority) {
     alert("Please enter a todo");
     return;
-}
+  }
 
+  const id = Math.floor(Math.random() * 10000000000000001);
 
-let id = Math.floor(Math.random() * 10000000000000001);
-const todo ={
+  const todo = {
     id,
     todoText,
-    priority
-};
+    priority,
+  };
 
-//  ul.appendChild(ele)
-
-  fetch("/todo",{
-    method:"POST",
-    headers:{"Content-Type":"application/json",},
-    body:JSON.stringify(todo), 
-  }).then(function(response){
-        if(response.status == 200){
-            // display todo in ui
-            showToDo(todo);
-        }
+  fetch("/addTodata", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  }).then(function(response) {
+    if (response.status === 200) {
+      showToDoSorted(todo); // Use the new function for showing the todo
+    }
   });
 });
 
 
 
-function showToDo(todo){
-    const todoTextNode=document.createElement("div");
-    todoTextNode.innerText=todo.todoText;
+
+function showToDoSorted(todo) {
+  const newTodoItem = document.createElement("div");
+  newTodoItem.innerText = todo.todoText;
 
 
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit-btn");
-    editBtn.innerText = "Edit";
+  const img = document.createElement('img');
+  img.setAttribute('src', todo.filename );
+  img.setAttribute('width', "50" );
+  img.setAttribute('height', "50" );
+  img.setAttribute('style', "max-width: 100%; max-height: 100%; object-fit: cover; margin-left: 30px; margin-right: 10px" );
 
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete-btn");
-    deleteBtn.innerText = "Delete";
 
+  const priorityBtn = document.createElement("button");
+  priorityBtn.innerText = todo.priority;
 
-    ul.appendChild(todoTextNode);
-    todoTextNode.appendChild(editBtn);
-    todoTextNode.appendChild(deleteBtn);
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-btn");
+  editBtn.innerText = "Edit";
 
-    // delete button functioning
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.innerText = "Delete";
 
-    deleteBtn.addEventListener("click",()=>{
-        const ID ={
-            id:todo.id
-        }
-        fetch("/delTodo",{
-            method:"POST",
-            headers:{"Content-Type":"application/json"},
-            body:JSON.stringify(ID) 
-        }).then((response)=>{
-            if(response.status === 200){
-                window.location.reload();
-            }
-            else{
-                console.log(response.json())
-                alert('Something wrong in todo')
-            }
-        });
-    });
+  newTodoItem.appendChild(editBtn);
+  newTodoItem.appendChild(deleteBtn);
+  newTodoItem.appendChild(priorityBtn);
+  newTodoItem.appendChild(img);
 
+  insertSorted(newTodoItem); // Insert the new todo in the sorted list
 
-    editBtn.addEventListener("click",()=>{
-        const edit=prompt("What is your next task..?");
+  // Add event listeners for delete and edit buttons
+  deleteBtn.addEventListener("click", () => {
+    deleteTodoById(todo.id);
+  });
 
-        if(edit){
-
-            const to={
-                id:todo.id,
-                data:edit
-            }
-            // var a = todo;
-            fetch("/editTodo",{
-                method:"POST",
-                headers:{"Content-Type":"application/json"},
-                body:JSON.stringify(to)
-            }).then((response)=>{
-                if(response.status === 200){
-                    window.location.reload();
-                }
-                else{
-                    // console.log(response.json());
-                    alert('Something wrong in todo')
-                }
-            });
-        }
-    });
-
+  editBtn.addEventListener("click", () => {
+    editTodoById(todo.id);
+  });
 }
 
 
-
-
-fetch("/todo-data").then((response)=>{
-    if(response.status ===200){
-        return response.json();
+function insertSorted(newTodoItem) {
+    const priority = newTodoItem.querySelector("button").innerText;
+    let insertIndex = 0;
+  
+    // Sort todos based on priority and insert accordingly
+    for (let i = 0; i < ul.children.length; i++) {
+      const currentPriority = ul.children[i].querySelector("button").innerText;
+      if (priority === "High" && currentPriority !== "High") {
+        insertIndex = i;
+        break;
+      } else if (priority === "Medium" && currentPriority === "Low") {
+        insertIndex = i;
+        break;
+      } else if (priority === "Low") {
+        insertIndex = i + 1;
+        break;
+      }
     }
-    else{
-        alert("something went wrong");
+  
+    // Insert the new todo at the correct position
+    ul.insertBefore(newTodoItem, ul.children[insertIndex]);
+  }
+
+function deleteTodoById(todoId) {
+  const ID = { id: todoId };
+  fetch("/delTodo", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(ID)
+  }).then((response) => {
+    if (response.status === 200) {
+      window.location.reload();
+    } else {
+      // console.log(response.json());
+      alert('Something went wrong in todo deletion');
     }
-}).then((todos)=>{
-    todos.forEach((todo) => {
-        showToDo(todo);
+  });
+}
+
+function editTodoById(todoId) {
+  const edit = prompt("What is your next task..?");
+
+  if (edit) {
+    const to = {
+      id: todoId,
+      data: edit
+    };
+    fetch("/editTodo", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(to)
+    }).then((response) => {
+      if (response.status === 200) {
+        window.location.reload();
+      } else {
+        // console.log(response.json());
+        alert('Something went wrong in todo editing');
+      }
     });
-}); 
+  }
+}
+
+fetch("/todo-data").then((response) => {
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    alert("Something went wrong");
+  }
+}).then((todos) => {
+  // Sort the todos based on priority before showing them
+  todos.sort((a, b) => {
+    const priorityA = a.priority.toLowerCase();
+    const priorityB = b.priority.toLowerCase();
+    if (priorityA < priorityB) return -1;
+    if (priorityA > priorityB) return 1;
+    return 0;
+  });
+
+  todos.forEach((todo) => {
+    showToDoSorted(todo);
+  });
+})
